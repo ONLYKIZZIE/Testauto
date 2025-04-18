1a520ce576ab154de30e0f03310f1dc4b91932d1
ONLYKIZZIE <jhaprabhakar212@gmail.com>
2025-04-18 19:15:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://skeleton100005:p7b6jTcUUv
 DB_NAME = os.environ.get("DATABASE_NAME", "skeleton100005")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002386159437"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002066259505"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002386159437"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
