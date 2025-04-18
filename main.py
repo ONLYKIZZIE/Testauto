f320ab98270389990e28cb838270c5215991fd21
ONLYKIZZIE <jhaprabhakar212@gmail.com>
2025-04-18 20:17:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 from plugins.auto_post import process_queue
 
-app = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins={"root": "plugins"})
+app = Client("bot", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins={"root": "plugins"})
 
 if __name__ == "__main__":
     app.run(process_queue(app))
